d6e974c09be85485377d5d80e8e5c5185a101da5
Ubuntu <ubuntu@ip-172-31-52-25.ec2.internal>
2018-02-02 02:19:22 +0000

    Fix run search script

--- run_search.sh
+++ run_search.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Searching mobilenet..."
-for i in {12..21} ;
+#echo "Searching mobilenet..."
+for i in {12..28} ;
 do
-    ( TVM_NUM_THREADS=4 python conv2d_nchw_x86_search.py --index $i --target "llvm -mcpu=skylake-avx512" & )
+    TVM_NUM_THREADS=4 python conv2d_nchw_x86_search.py --index $i --target "llvm -mcpu=skylake-avx512" & 
+done
+wait
+for i in {39..55} ;
+do
+    TVM_NUM_THREADS=4 python conv2d_nchw_x86_search.py --index $i --target "llvm -mcpu=skylake-avx512" & 
 done
